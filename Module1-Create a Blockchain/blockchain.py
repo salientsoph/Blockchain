530cea9503a1c5f4b808d8a3e97200d9b60a6cb3
salientsoph <salientsoph@gmail.com>
2023-01-06 21:24:41 +0900

    20230106 pow&hash

--- Module1-Create a Blockchain/blockchain.py
+++ Module1-Create a Blockchain/blockchain.py
@@ -24,9 +24,32 @@ Class Blockchain:
     def create_block(self, proof, previous_hash):
         block = {'index' : len(self.chain) + 1,
                  'timestamp': str(datetime.date.now()),
-                 'proof': 
+                 'proof': proof,
+                 'previous_hash': previous_hash
                  }
+        self.chain.append(block)
+        return block
+    
+    def get_previous_block(self):
+        return self.chain[-1]
 
+    def proof_of_work(self, previous_proof):
+        new_proof = 1
+        check_proof = False
+        while check_proof is False:
+            hash_operation = hashlib.sha256(str(new_proof**2 + previous_proof**2).encode()).hexadigest()
+            if hash_operation[:4] == '0000':
+                check_proof = True
+            else:
+                new_proof += 1 
+        return new_proof
+            
+    def hash(self, block):
+        encoded_block = json.dumps(block, sort_keys=true).encode()
+        return hashlib.sha256(encoded_block).hexdigest()
+        
+        
+        
 # Part 2 - Mining our Blockchain
 
 
